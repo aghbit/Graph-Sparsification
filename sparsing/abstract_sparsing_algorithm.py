a3aaf86a7d12e56b498ffb1ebd526864dc7a9efa
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2024-05-31 17:24:29 +0200

    Updated the notebook for plotting

--- sparsing/abstract_sparsing_algorithm.py
+++ sparsing/abstract_sparsing_algorithm.py
@@ -18,14 +18,15 @@ class BaseSparsing(BaseTransform):
 
 
 class IndexMain(BaseSparsing):
-    def __init__(self, power: int = None, calc=None):
+    def __init__(self, power: int=None, calc=None, calc_name=None):
         super(IndexMain, self).__init__(power=power)
         self._calc = calc
+        self._calc_name = calc_name
 
     def _main_calc(self, G):
         return self._calc.run(G)
 
-    def __call__(self, data: Data) -> Data:
+    def __call__(self, data: Data, data_name: str, data_type: str) -> Data:
         if self.power is not None:
             edge_index = data.edge_index
             mask = edge_index[0] < edge_index[1]
@@ -33,7 +34,7 @@ class IndexMain(BaseSparsing):
             G = to_networkx(data, to_undirected=True)
             G = nx2nk(G)
             edge_weights = self._main_calc(G)
-            # show_cdf(edge_weights)
+            #show_cdf(edge_weights, data_name, data_type, self._calc_name)
             edge_index = edge_index[:, edge_weights >= self.power]
             edge_index = torch.cat([edge_index, torch.flip(edge_index, dims=[0])], dim=1)
             data.edge_index = edge_index
