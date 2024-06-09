41d3b1d5f015d284aad14d5d51bc9c550c805fbc
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2024-06-09 17:23:50 +0200

    Set minmax to True and fixed power values

--- sparsing/sparsing_algorithms.py
+++ sparsing/sparsing_algorithms.py
@@ -3,47 +3,47 @@ from sparsing.abstract_sparsing_algorithm import IndexMain
 
 
 class Jaccard(IndexMain):
-    def __init__(self, power: int = None, calc=JaccardCalc()):
+    def __init__(self, power: int = None, calc=JaccardCalc(minmax=True)):
         super(Jaccard, self).__init__(power=power, calc=calc)
 
 
 class CommonNeighbor(IndexMain):
-    def __init__(self, power: int = None, calc=CommonNeighborsCalc()):
+    def __init__(self, power: int = None, calc=CommonNeighborsCalc(minmax=True)):
         super(CommonNeighbor, self).__init__(power=power, calc=calc)
 
 
 class PreferentialAttachment(IndexMain):
-    def __init__(self, power: int = None, calc=PreferentialAttachmentCalc()):
+    def __init__(self, power: int = None, calc=PreferentialAttachmentCalc(minmax=True)):
         super(PreferentialAttachment, self).__init__(power=power, calc=calc)
 
 
 class AdamicAdar(IndexMain):
-    def __init__(self, power: int = None, calc=AdamicAdarCalc()):
+    def __init__(self, power: int = None, calc=AdamicAdarCalc(minmax=True)):
         super(AdamicAdar, self).__init__(power=power, calc=calc)
 
 
 class AdjustedRand(IndexMain):
-    def __init__(self, power: int = None, calc=AdjustedRandCalc()):
+    def __init__(self, power: int = None, calc=AdjustedRandCalc(minmax=True)):
         super(AdjustedRand, self).__init__(power=power, calc=calc)
 
 
 class AlgebraicDistance(IndexMain):
-    def __init__(self, power: int = None, calc=AlgebraicDistanceCalc()):
+    def __init__(self, power: int = None, calc=AlgebraicDistanceCalc(minmax=True)):
         super(AlgebraicDistance, self).__init__(power=power, calc=calc)
 
 
 class Katz(IndexMain):
-    def __init__(self, power: int = None, calc=KatzCalc()):
+    def __init__(self, power: int = None, calc=KatzCalc(minmax=True)):
         super(Katz, self).__init__(power=power, calc=calc)
 
 
 class LDS(IndexMain):
-    def __init__(self, power: int = None, calc=LDSCalc()):
+    def __init__(self, power: int = None, calc=LDSCalc(minmax=True)):
         super().__init__(power=power, calc=calc)
 
 
 class ForestFire(IndexMain):
-    def __init__(self, power: int = None, calc=FFSCalc()):
+    def __init__(self, power: int = None, calc=FFSCalc(minmax=True)):
         super().__init__(power=power, calc=calc)
 
 
@@ -61,14 +61,14 @@ sparsing_list = [
     (AdamicAdar, "AdamicAdar", [0.01, 0.02, 0.04, 0.06, 0.08, 0.1]),
     (AdjustedRand, "AdjustedRand", [0.01, 0.02, 0.04, 0.06, 0.08, 0.1]),
     #(AlgebraicDistance, "AlgebraicDistance", [0.01, 0.02, 0.04, 0.06, 0.08, 0.1]),
-    #(Katz, "Katz", [-3, -2.5, -2, -1.5, -1]),
+    (Katz, "Katz", [0.01, 0.02, 0.04, 0.06, 0.08, 0.1]),
     # (Jaccard, "JaccardIndex", NORM_TAB),
     # (CommonNeighbor, "CommonNeighborIndex", NORM_TAB),
     # (PreferentialAttachment, "PreferentialAttachment", NORM_TAB),
     # (AdamicAdar, "AdamicAdar", NORM_TAB),
     # (AdjustedRand, "AdjustedRand", NORM_TAB),
     #(AlgebraicDistance, "AlgebraicDistance", NORM_TAB),
-    (Katz, "Katz", NORM_TAB),
+    #(Katz, "Katz", NORM_TAB),
     (LDS, "LocalDegreeScore", [0.01, 0.02, 0.04, 0.06, 0.08, 0.1]),
     (ForestFire, "ForestFire", [0.01, 0.02, 0.04, 0.06, 0.08, 0.1]),
 ]
