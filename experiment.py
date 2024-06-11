8e14e405ad98bfed7ff23db83b2c655c1720b9b7
Krzysztof Gwiazda <406841@student.agh.edu.pl>
2024-06-11 14:27:03 +0200

    HF

--- experiment.py
+++ experiment.py
@@ -25,7 +25,7 @@ def run_exp(dataset, model, sparsing_alg=None):
         loss.backward()
         optimizer.step()
     model.eval()
-    pred = model(data).argmax(dim=1)
+    pred = model(data.x, data.edge_index).argmax(dim=1)
     correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
     acc = int(correct) / int(data.test_mask.sum())
     return acc
