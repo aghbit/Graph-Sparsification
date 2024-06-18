56e55e1f08b00954e3affef05971fec0579cded3
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2024-06-18 14:20:58 +0200

    Cosmetic changes

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_model(model_type, dataset):
 
 
 if __name__ == '__main__':
-    run_num = 1
+    run_num = 10
     results = []
     for dataset in datasets:
         for model_data in models:
