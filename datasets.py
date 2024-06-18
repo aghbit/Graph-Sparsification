bf2f0a93201a672510e9d8ac4347e54a31ec33f7
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2024-06-18 15:58:31 +0200

    Fixed sparsification edge count and changed n_epochs to 200

--- datasets.py
+++ datasets.py
@@ -2,8 +2,8 @@ from torch_geometric.datasets import *
 
 datasets = [
     Planetoid(root='/tmp/Cora', name='Cora'),
-    Planetoid(root='/tmp/CiteSeer', name='CiteSeer'),
-    Planetoid(root='/tmp/PubMed', name='PubMed'),
+    # Planetoid(root='/tmp/CiteSeer', name='CiteSeer'),
+    # Planetoid(root='/tmp/PubMed', name='PubMed'),
     # Coauthor(root='/tmp/Physics', name='Physics'),
     # Coauthor(root='/tmp/ComputerScience', name='CS'),
     #CitationFull(root='/tmp/CoraFull', name='Cora'),
