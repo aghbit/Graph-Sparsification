404ab18aa69d8f890a58a0a9a015aee627f78096
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2024-07-27 12:49:46 +0200

    Added scripts for experiments on each heterophilous dataset

---
+++ heterophily/scripts/run_experiments_questions.sh
@@ -0,0 +1,7 @@
+for SPARSIFICATION in "PreferentialAttachment" "AdjustedRand" "LDS" "LSS" "SCAN"
+do
+  for POWER in 1 2 3 4 5
+  do
+    python train.py --name "SAGE-$SPARSIFICATION-$POWER" --dataset questions --model SAGE --num_layers 5 --device cuda:0 --sparsification_algorithm $SPARSIFICATION --sparsification_power $POWER
+  done
+done
